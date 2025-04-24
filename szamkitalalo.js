486689ceb304900a2a2c982eb45fd73fd56e5cf9
hajasko <hajasko87@gmail.com>
2025-04-24 16:13:25 +0200

    target generálása

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -1,1 +1,18 @@
-alert ('Hello');
+let target;
+
+function generateTarget() {
+    return Math.floor(Math.random()*100)+1;
+}
+
+function startGame() {
+    target = generateTarget();
+}
+
+startGame();
+
+// document.getElementById('newGame').addEventListener('click', (event) => {
+//     event.preventDefault();
+//     generateNumber();
+// });
+
+// document.onload = generateNumber();
