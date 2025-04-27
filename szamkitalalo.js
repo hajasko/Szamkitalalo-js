09b700489f0aa106356e76bf043e32e82c9c48e4
hajasko <hajasko87@gmail.com>
2025-04-27 07:55:10 +0200

    tippek kiértékelése, Game Over

--- szamkitalalo.js
+++ szamkitalalo.js
@@ -4,6 +4,14 @@ let newGameButton = document.querySelector('.js-new-game-button');
 let guessSubmitButton = document.querySelector('.js-submit-guess');
 
 let target;
+let guessList = [];
+let gameOver = false;
+
+const guessResults = {
+    KISEBB: 'kisebb',
+    NAGYOBB: 'nagyobb',
+    HELYES: 'talált'
+}
 
 function generateTarget() {
     return Math.floor(Math.random()*100)+1;
@@ -11,9 +19,12 @@ function generateTarget() {
 
 function resetGame() {
     // korábbi tippek törlése
-    guessListContainer.innerHTML = '';
+    guessList = [];
+    renderGuessList();
     // input mező tartalmának törlése
     currentGuessField.value = '';
+    // engedünk tippelni azzal, hogy nincs vége a játéknak
+    gameOver = false;
 }
 
 function startGame() {
@@ -21,14 +32,57 @@ function startGame() {
     target = generateTarget();
 }
 
+function renderGuessList() {
+    let html = '';
+    let guessType, guessText;
+
+    for (let {guessValue, result} of guessList) {
+        if (result === guessResults.KISEBB) {
+            guessType = "warning";
+            guessText = "a keresett szám kissebb."
+        } else if (result === guessResults.NAGYOBB) {
+            guessType = "primary";
+            guessText = "a keresett szám nagyobb."
+        } else {
+            guessType = "success";
+            guessText = `Gratulálok, eltaláltad! Tippek száma: ${guessList.length}`
+        }
+        html += `<div class="row">
+                    <div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4 alert alert-${guessType}" role="alert">
+                        ${guessValue} - ${guessText} 
+                    </div>
+                </div>`
+    }
+
+    guessListContainer.innerHTML = html;
+}
+
+function addGuess(guessValue, result) {
+    guessList.unshift({guessValue, result});
+}
+
+function compareGuessToTarget(guess) {
+    if (guess < target) {
+        addGuess(guess, guessResults.NAGYOBB);
+    } else if (guess > target) {
+        addGuess(guess, guessResults.KISEBB);
+    } else {
+        addGuess(guess, guessResults.HELYES);
+        gameOver = true;
+    }   
+}
+
 function submitGuess(event) {
     event.preventDefault();
-    let guessValue = Number(currentGuessField.value);
+    if (!gameOver) {
+        let guessValue = Number(currentGuessField.value);
 
-    console.log(guessValue, typeof guessValue);
-    if (validateGuess()) {
-        alert('klikk');
+        if (validateGuess()) {
+            compareGuessToTarget(guessValue);
+            renderGuessList();
+        }
     }
+    
 }
 
 function addGuessValidation() {
